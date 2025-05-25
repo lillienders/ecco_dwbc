0d44d8c60e9c88c7d3a07ab3787a9042b8c11c7b
Lilli Enders <lilli.enders@outlook.com>
2025-05-25 22:03:24 +0000

    Add initial files from first days of SS

---
+++ proj_utils.py
@@ -0,0 +1,114 @@
+from os.path import join,expanduser
+import sys
+user_home_dir = expanduser('~')
+sys.path.insert(0,join(user_home_dir,'ECCOv4-py'))
+import ecco_v4_py as ecco
+import botocore  
+import boto3
+from boto3.session import Session
+import cmocean
+from collections import Counter
+from dask.distributed import Client
+import datetime
+import fsspec
+from gc import get_referents
+import json
+import numpy as np
+from pathlib import Path
+from pprint import pprint
+import requests
+import s3fs
+import sys
+from sys import getsizeof
+import time as time
+from types import ModuleType, FunctionType
+import xarray as xr
+import matplotlib.pyplot as plt
+import zarr
+
+# Use this for the netcdf files stored on an s3 bucket
+def get_credentials(use_earthdata=False):
+    """
+    This routine automatically pulls your EDL crediential from .netrc file and use it to obtain an AWS S3 credential 
+    through a PO.DAAC service accessible at https://archive.podaac.earthdata.nasa.gov/s3credentials.
+    From the PO.DAAC Github (https://podaac.github.io/tutorials/external/July_2022_Earthdata_Webinar.html).
+    
+    Returns:
+    =======
+    
+    credentials: a dictionary with AWS secret_key, access_key, and token
+    """
+    # NASA EarthData hosts ECCO V4r4 fileds
+    if use_earthdata == False:
+        session = boto3.Session()
+        credentials_b3 = session.get_credentials()
+        creds_b3 = credentials_b3.get_frozen_credentials()
+        
+        credentials = dict()
+        credentials['secretAccessKey'] = credentials_b3.secret_key
+        credentials['accessKeyId'] = credentials_b3.access_key
+        credentials['sessionToken'] = credentials_b3.token
+
+    # A 'public' AWS s3 bucket hosts V4r5 fields (they will eventually move to PO.DAAC)
+    else:
+        credentials = requests.get('https://archive.podaac.earthdata.nasa.gov/s3credentials').json()
+    
+    return credentials
+
+def init_S3FileSystem(use_earthdata=False, requester_pays=True):
+    """
+    This routine automatically creates an 's3 file system' object and credentials dictionary.
+    The s3 file system needs to be initialized with the special aws credentials.
+    
+    Returns:
+    =======
+    
+    s3: an AWS S3 filesystem, 
+    credentials: a dictionary with AWS secret_key, access_key, and token
+
+    """
+    credentials = get_credentials(use_earthdata=use_earthdata)
+
+    if use_earthdata:
+        requester_pays = False
+        
+    s3 = s3fs.S3FileSystem(requester_pays=requester_pays,
+                           anon=False,
+                           key=credentials['accessKeyId'],
+                           secret=credentials['secretAccessKey'], 
+                           token=credentials['sessionToken'])
+    
+    return s3, credentials
+
+# function for determining the memory footprint of an object
+
+# ... from https://stackoverflow.com/questions/449560/how-do-i-determine-the-size-of-an-object-in-python
+
+# Custom objects know their class.
+# Function objects seem to know way too much, including modules.
+# Exclude modules as well.
+BLACKLIST = type, ModuleType, FunctionType
+
+def getsize(obj):
+    """
+    This routine returns the in-memory size of an python object
+    
+    Returns:
+    =======
+    
+    size: size of object & members.
+    """
+    if isinstance(obj, BLACKLIST):
+        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
+    seen_ids = set()
+    size = 0
+    objects = [obj]
+    while objects:
+        need_referents = []
+        for obj in objects:
+            if not isinstance(obj, BLACKLIST) and id(obj) not in seen_ids:
+                seen_ids.add(id(obj))
+                size += sys.getsizeof(obj)
+                need_referents.append(obj)
+        objects = get_referents(*need_referents)
+    return size
